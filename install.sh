946f8aac789dbec8e918996b4e22984e3b8e5956
Christopher Kapic <christopherkapic@gmail.com>
2022-03-24 21:35:09 -0500

    install node 16

--- install.sh
+++ install.sh
@@ -10,6 +10,13 @@ cp ~/.config/fonts/* ~/Library/Fonts
 
 # Install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Install NodeJS 16
+nvm install 16
+nvm use 16
 
 read -r -p "Install Homebrew? [y/N] " input
 
