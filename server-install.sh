f878194381f20a981fafc78984ed01999efde616
Christopher Kapic <christopherkapic@gmail.com>
2022-03-24 23:13:30 -0500

    install pip apparently?

--- server-install.sh
+++ server-install.sh
@@ -20,7 +20,7 @@ nvm install 16
 sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
-sudo apt-get install neovim
+sudo apt-get install neovim pip zsh
 
 # install Rust for LunarVim
 # Official:
