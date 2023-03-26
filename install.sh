0d3ed8acf006dc4c21229681d3b9985c49ae2186
Christopher Kapic <christopherkapic@Christophers-MacBook-Pro.local>
2023-03-26 11:21:44 -0500

    Conditionally install nvm, node

--- install.sh
+++ install.sh
@@ -6,18 +6,24 @@ git clone --depth=1 https://github.com/christopher-kapic/dotfiles.git ~/.config
 cp ~/.config/zsh/.zshrc ~/.zshrc
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 
 cp $HOME/.config/fonts/* $HOME/Library/Fonts
 
 # Install nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+if ! command -v nvm &> /dev/null
+then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
 
-# Install NodeJS 16
-nvm install 16
+if ! command -v node &> /dev/null
+then
+  # Install NodeJS 16
+  nvm install 18
+fi
+
 
 if ! command -v brew &> /dev/null
 then
@@ -47,18 +53,11 @@ then
     exit
 fi
 
-# install Rust for LunarVim
-# Official:
-# curl https://sh.rustup.rs -sSf | sh
-
 bash <(curl -s https://sh.rustup.rs)
 source $HOME/.cargo/env
 
-# Install LunarVim
-# Official:
-# bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
-
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
-# curl https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh | bash
-# curl -o- https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh | bash
-# bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+
+echo "Configuring some settings..."
+echo "Set dock speed to 0.2s (1/2 of Doherty threshold)"
+defaults write com.apple.dock autohide-delay -float 0; defaults write com.apple.dock autohide-time-modifier -float 0.2;killall Dock
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -95,3 +95,5 @@ source ~/.config/shell/env
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+source ~/powerlevel10k/powerlevel10k.zsh-theme
