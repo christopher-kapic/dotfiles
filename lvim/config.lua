7f69554f5243bd94ddba5622ab08bcab7649c1fe
Christopher Kapic <christopherkapic@gmail.com>
2023-11-17 14:46:13 -0600

    Updates

--- lvim/config.lua
+++ lvim/config.lua
@@ -20,6 +20,7 @@ lvim.transparent_window = true
 lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode.m = function() MiniMap.toggle() end
 -- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 -- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 -- unmap a default keymapping
