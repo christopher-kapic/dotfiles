9d47b30b7a3c8300a1d1c8b3305fbbda51404852
Christopher Kapic <christopherkapic@gmail.com>
2024-01-16 15:59:30 -0600

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-input_directory=$1
+# input_directory=$1
 
-if [ -z "$input_directory" ]; then
-  input_directory=$(pwd)
-fi
+# if [ -z "$input_directory" ]; then
+#   input_directory=$(pwd)
+# fi
 
-echo "This is saving files to $input_directory"
+# echo "This is saving files to $input_directory"
 echo "This is not actually saving files, it is just a demo."
