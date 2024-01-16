cdee24d0b5e21d0129df808c7707068b89b84e3f
Christopher Kapic <christopherkapic@gmail.com>
2024-01-16 15:54:36 -0600

    Create example.sh

---
+++ example.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+input_directory=$1
+
+if [ -z "$input_directory" ]; then
+  input_directory=$(pwd)
+fi
+
+echo "This is saving files to $input_directory"
+echo "This is not actually saving files, it is just a demo."
