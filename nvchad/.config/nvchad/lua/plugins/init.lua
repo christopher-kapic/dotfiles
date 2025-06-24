52479ea867f873015b1df4eb3eba7f71b211b99f
ck <christopherkapic@gmail.com>
2025-06-24 08:21:12 -0500

    Update init.lua

--- nvchad/.config/nvchad/lua/plugins/init.lua
+++ nvchad/.config/nvchad/lua/plugins/init.lua
@@ -12,6 +12,22 @@ return {
       require "configs.lspconfig"
     end,
   },
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup {
+        filters = {
+          dotfiles = false,
+          git_ignored = false,
+        }
+      }
+    end,
+  },
 
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
